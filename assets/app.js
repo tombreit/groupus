7cf139289c0e1d6c59ae8a40e942f6de18420730
Thomas Breitner <mail@thms.de>
2023-07-09 23:07:03 +0200

    new person item layout

--- assets/app.js
+++ assets/app.js
@@ -1,7 +1,10 @@
 // import * as bootstrap from 'bootstrap';
+import "./app.scss";
 import Sortable from 'sortablejs';
 import htmx from 'htmx.org';
 
+
+const personsObj = new Set();
 
 const calculatorWrapperElement = document.getElementById('calculator-wrapper');
 const prefsParentElement = document.getElementById('prefsContainer');
@@ -13,48 +16,31 @@ const personsNamesInputField = document.getElementById("personsNamesInputField")
 personsPrefsFormElement.addEventListener('click', getPersonPrefs);
 personsNamesInputField.addEventListener('keyup', convertToElements);
 
-prefsParentElement.addEventListener('click', function(e){
-    if (e.target.classList.contains("pref-dismiss")) {
-        e.target.parentElement.remove();
+// Helper function to attach event listener to every child element of target
+const on = (element, type, selector, handler) => {
+  element.addEventListener(type, (event) => {
+    if (event.target.closest(selector)) {
+      handler(event);
     }
-})
+  });
+};
 
-
-const personsObj = new Set();
-
-new Sortable(personsContainer, {
-    group: {
-      name: "shared",
-        pull: 'clone',
-        put: false,
-    },
-    sort: false,
-    animation: 150
-});
-
-
-function createPersonElemMarkup(person, isActive) {
-    const active = isActive ? "active" : "";
-    return `
-        <div 
-            class="pref-item draggable text-nowrap list-group-item ${active}" 
-            data-id="${person}"
-        >
-            ${person} 
-            <span class="pref-dismiss">✖</span>
-        </div>
-    `
+function dismissPrefElem(elem){
+  const prefItemElem = elem.closest(".pref-item");
+  prefItemElem.remove();
 }
+
+on(document, 'click', '.pref-dismiss', (event) => dismissPrefElem(event.target));
 
 
 function getPersonPrefs(){
-  // console.log('Executing before form submission...');
-
+  // Before form submission, collect and return the prefs for each person
+  // and store them in the forms hidden textarea input field.
   const personsPrefsObjs = []
 
   for (personPref of prefsParentElement.children) {
       const dataAttributeValues = [];
-      const listItems = personPref.querySelectorAll('.list-group-item');
+      const listItems = personPref.querySelectorAll('.pref-item');
 
       listItems.forEach(item => {
         const dataAttributeValue = item.getAttribute('data-id');
@@ -63,7 +49,6 @@ function getPersonPrefs(){
       personsPrefsObjs.push(dataAttributeValues)
   }
 
-  // console.info("personsPrefsObjs: ", personsPrefsObjs)
   personsPrefsInputElement.value = JSON.stringify(personsPrefsObjs);
 };
 
@@ -73,18 +58,46 @@ function convertToElements() {
 
     inputValues.forEach(function(value) {
         value = value.trim();
-        // console.log(personsNamesInputField.value)
 
         if (personsNamesInputField.value.endsWith(",")){         
           if (value) {
-            // console.info(value);
             personsObj.add(value);
-            // console.log(personsObj);
             buildPrefsTable(personsObj);
           };
         };
         
     });
+}
+
+
+new Sortable(personsContainer, {
+    group: {
+      name: "shared",
+        pull: "clone",
+        put: false,
+    },
+    sort: false,
+    animation: 150
+});
+
+
+function createPersonElemMarkup(person, isActive) {
+    const active = isActive ? "active disabled" : "";
+    // const draggable = isActive ? "": "draggable";
+
+    return `
+    <span 
+      class="pref-item text-nowrap ${active}"
+      data-id="${person}"
+    >
+      <i class="person-avatar bi bi-person-circle me-2"></i>
+      ${person} 
+      <span class="pref-dismiss">
+        <span class="vr mx-1"></span>
+        <i class="bi bi-x-circle-fill"></i>
+      </span>
+    </span>
+    `
 }
 
 
@@ -94,14 +107,14 @@ function buildPrefsTable(personsObj) {
   prefsParentElement.replaceChildren();
 
   for (person of personsObj) {
-    let active = "noactive";
+    // Build the persons list as draggable source
     let personElement = createPersonElemMarkup(person, false);
     personsParentElement.insertAdjacentHTML("beforeend", personElement);
 
-    active = "asdfadsactive";
+    // Build the prelimary empty prefs container for each person
     const prefsContainer = `
-        <div class="card m-4">
-            <div id="prefs-${person}" class="prefs-list list-group list-group-flush">
+        <div class="card m-4 p-2">
+            <div id="prefs-${person}" class="prefs-list">
                 ${createPersonElemMarkup(person, true)}
             </div>
         </div>
@@ -111,14 +124,36 @@ function buildPrefsTable(personsObj) {
 
   const prefsSortable = document.getElementById(`prefs-${person}`);
   const sortable = Sortable.create(prefsSortable, {
-    animation: 150,
+    // TODO: Prevent adding an item at the first position of the list.
+    // This position is already taken by the hardcoded person item.
+    // onAdd: function (evt) {
+    //   console.log("onAdd: ", evt)
+    //   console.log("firstItem: ", firstItem)
+    //   if (evt.newIndex === 0) {
+    //     console.log("evt.newIndex === 0");
+    //     evt.preventDefault(); // Prevent dropping the item at the first position
+    //   }
+    // },
+    onMove: function (evt) {
+      // Do not allow an element to be moved before .active element
+      return evt.related.className.indexOf('active') === -1;
+    },
+    filter: ".active",
     group: {
       name: "shared",
-      pull: 'clone',
+      pull: "clone",
         sort: true,
-        put: (to, from, dragEl, event) => { for (let i = 0; i < to.el.children.length; i++) { if (to.el.children[i].getAttribute("data-id") === dragEl.getAttribute("data-id")) { console.log("already exist"); return false } } return true }
+        put: (to, from, dragEl, event) => { 
+          // Prevent adding the already hardcoded person item
+          for (let i of to.el.children) {
+            if (i.getAttribute("data-id") === dragEl.getAttribute("data-id")) { 
+              console.log("Person already exist in this prefs list!"); 
+              return false
+            }
+          } 
+          return true 
+        }
     },
   });
   };
 };
-
