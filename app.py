75b373d58b70ad60b04a1b09d5bc9ed514e61566
Thomas Breitner <mail@thms.de>
2023-07-20 22:56:34 +0200

    cleanup

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import argparse
 import csv
 import json
-import numpy as np
 import pandas as pd
 from flask import Flask, render_template, request
 from algorithm import Matching
@@ -32,29 +31,16 @@ def calculate():
 
 
 def create_prefs_matrix(students, prefs):
-    # print("prefs:")
-    # print(prefs)
-
-    # students_len = len(students)
-    # array = np.zeros((students_len, students_len), dtype=np.int16)          
     df_with_headers = pd.DataFrame(0, index=students, columns=students)
 
     for pref in prefs:
         for_student = pref.pop(0)
         weights = list(reversed(range(11)))
-        # print(f"Setting preferences for {for_student=}")
+
         for student in pref:
-            # print(f"{student=}")
             df_with_headers.at[for_student, student] = weights.pop(0)
 
-    # print("df_with_headers:")
-    # print(df_with_headers)
-    # print(type(df_with_headers))
-
     np_prefs_array = df_with_headers.to_records(index=False)
-    # print("np_prefs_array:")
-    # print(np_prefs_array)
-
     return np_prefs_array
 
 
@@ -73,16 +59,9 @@ def get_groups_from_csv(csvfile, group_size):
 
 
 def get_groups(prefs, group_size):
-    print(f"{prefs=}")
-    print(f"{type(prefs)=}")
-
     prefs = json.loads(prefs)
-    # print(f"{group_size=}")
-
     students = [pref[0] for pref in prefs]
     prefs_matrix = create_prefs_matrix(students, prefs)
-    # print("prefs_matrix: ")
-    # print(prefs_matrix)
 
     matching = Matching(
         prefs_matrix,
@@ -92,13 +71,9 @@ def get_groups(prefs, group_size):
     )
     score, groups = matching.solve()
 
-    # print(f"{groups=}")
-    # print(f"{prefs=}")
     named_groups = []
-
     for group in groups:
         named_group = [students[student_index] for student_index in group]
-        print(f"{named_group=}")
         named_groups.append(named_group)
 
     return {
